dd0dfaa71653b7f128fa8155bf784b91ecd3c6b1
mwarnsley <marcus.warnsley@gmail.com>
2016-10-14 21:50:39 -0400

    Editing some of the structure and functions

--- components/PieChart.js
+++ components/PieChart.js
@@ -15,60 +15,58 @@ const PiePaths = React.createClass({
           { name: 'Safari', count: 14 },
           { name: 'Firefox', count: 9 },
           { name: 'Others', count: 6 }
-      ];
-          var arc = d3.svg.arc()
-              .outerRadius(0)
-              .innerRadius(300);
-              var pie = d3.layout.pie()
-                        .value(function(d){return d.count});
-
-              var color = d3.scale.ordinal()
-                        .range([
-                          '#' + Math.floor(Math.random() * 16777215).toString(16),
+        ],
+            arc = d3.svg.arc()
+                    .outerRadius(0)
+                    .innerRadius(300),
+            pie = d3.layout.pie()
+                    .value((d) =>{
+                      return d.count
+                    }),
+            color = d3.scale.ordinal()
+                    .range([
+                        '#' + Math.floor(Math.random() * 16777215).toString(16),
                         '#' + Math.floor(Math.random() * 16777215).toString(16),
                         '#' + Math.floor(Math.random() * 16777215).toString(16),
                         '#' + Math.floor(Math.random() * 16777215).toString(16),
                         '#' + Math.floor(Math.random() * 16777215).toString(16),
                         '#' + Math.floor(Math.random() * 16777215).toString(16)
-                      ]);
+                      ]),
+            transform = 'translate(400, 300)',
+            paths = (pie(data)).map((d, i) => {
+                                      return (
+                                          <path fill={color(i)} d={arc(d)} key={i}/>
+                                      )
+                                  });
 
+                                    return(
+                                        <g transform={transform}>
+                                            {paths}
+                                        </g>
+                                    )
+                                }
+                            });
+                            const PieChart = React.createClass({
+                              getDefaultProps(){
+                                return {
+                                  svgHeight: 700,
+                                  svgWidth: 700
+                                }
+                              },
 
-
-          var transform = 'translate(400, 300)';
-      var paths = (pie(data)).map(function(d, i) {
-          return (
-              <path fill={color(i)} d={arc(d)} key={i}/>
-          )
-      });
-
-        return(
-            <g transform={transform}>
-                {paths}
-            </g>
-        )
-    }
-});
-const PieChart = React.createClass({
-  getDefaultProps(){
-    return {
-      svgHeight: 700,
-      svgWidth: 700
-    }
-  },
-
-  render() {
-          return(
-            <svg
-              height={this.props.svgHeight}
-              width={this.props.svgWidth}
-            >
-            <PiePaths
-              width={600}
-              height={400}
-            />
-            </svg>
-          );
-  }
-});
+                              render() {
+                                      return(
+                                        <svg
+                                          height={this.props.svgHeight}
+                                          width={this.props.svgWidth}
+                                        >
+                                        <PiePaths
+                                          width={600}
+                                          height={400}
+                                        />
+                                        </svg>
+                                      );
+                              }
+                            });
 
 export default PieChart;
